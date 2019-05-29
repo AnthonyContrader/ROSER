9e75cc68dd18801741eec96efeec0fbe55bf7f08
Fiestmo <c.scognamiglio@contrader.it>
2019-05-29 18:14:49 +0100

    UpdateDTO

--- src/main/java/it/contrader/dto/UserDTO.java
+++ src/main/java/it/contrader/dto/UserDTO.java
@@ -2,8 +2,56 @@ package it.contrader.dto;
 
 public class UserDTO {
 	private int userId;
+	
 	private String username;
 	private String usertype;
+	private String name;
+	private String surname;
+	private String password;
+	private boolean userState;
+	
+	public UserDTO(String name,String surname,String username,String password,String usertype,boolean userState)
+	{
+		this.name = name;
+		this.surname = surname;
+		this.username = username;
+		this.password = password;
+		this.usertype = usertype;
+		this.userState = userState;
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isUserState() {
+		return userState;
+	}
+
+	public void setUserState(boolean userState) {
+		this.userState = userState;
+	}
 
 	public UserDTO(String username, String usertype) {
 		this.username = username;
@@ -34,6 +82,11 @@ public class UserDTO {
 
 	public void setUsertype(String usertype) {
 		this.usertype = usertype;
+	}
+	
+	public void ShowAttributes()
+	{
+		System.out.println("nome:" + this.name + " " +"Cognome: "+ this.surname +" "+ "username: "+ this.username + " "+"password: "+this.password+ " " );
 	}
 
 }
