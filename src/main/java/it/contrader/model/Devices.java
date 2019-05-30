cbd7d909027c531e5db8ef18c6d5934397d98fda
MicaelVanini <m.vanini@contrader.it>
2019-05-30 17:35:04 +0200

    Update Devices Model

--- src/main/java/it/contrader/model/Devices.java
+++ src/main/java/it/contrader/model/Devices.java
@@ -4,65 +4,42 @@ public class Devices {
 	private int devId;
 	private String model;
 	private User user;
+	
+	public Devices (int devId , String model , User user) {
+		this.model = model;
+		this.user = user;
+		this.devId = devId;
+	}
 
+	public Devices() {}
+	
+	
+	public int getDevId() {
+		return devId;
+	}
 
+	public void setDevId(int devId) {
+		this.devId = devId;
+	}
 
+	public String getModel() {
+		return model;
+	}
 
-public Devices (int devId , String model , User user) {
-	this.model = model;
-	this.user = user;
-	this.devId = devId;
-}
+	public void setModel(String model) {
+		this.model = model;
+	}
 
-public Devices() {}
+	public User getUser() {
+		return user;
+	}
 
-
-
-public int getDevId() {
-	return devId;
-}
-
-
-
-
-public void setDevId(int devId) {
-	this.devId = devId;
-}
-
-
-
-
-public String getModel() {
-	return model;
-}
-
-
-
-
-public void setModel(String model) {
-	this.model = model;
-}
-
-
-
-
-public String getUsername() {
-	return user.getUsername();
-}
-
-public User getUser()
-{
-	return this.user;
-}
-
-
-
-public void setUser(User user) {
-	this.user = user;
-}
-
-@Override
-public String toString() {
-	return this.getDevId() + "\t" + this.getModel() + "\t" + this.getUsername();
-}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getDevId() + "\t" + this.getModel() + "\t" + this.getUser().getUsername();
+	}
 }
